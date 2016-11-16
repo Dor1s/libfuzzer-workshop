70ad22f5cb307ada7cf394cc849544324e776e34
Max Moroz <balalaikacr3w@gmail.com>
2016-11-16 14:26:27 +0100

    Update requirements.

--- README.md
+++ README.md
@@ -9,15 +9,15 @@ The workshop will be hosted at [ZeroNights'16] security conference.
 * Linux-based OS
 * C/C++ experience (nothing special, but you need to be able to read, write and
 compile C/C++ code)
-* Optional requirement: a recent version of clang compiler. Distributions from
+* a recent version of clang compiler. Distributions from
 package managers are too old and most likely won't work (the workshop
 called "modern", right?), you have two options:
- * checkout **clang** repository and build it yourself
- * checkout **Chromium** repository and use the binaries located at
-`src/third_party/llvm-build/Release+Asserts/bin/`
-* **clang** and other binaries will be provided as the workshop materials, but if
-you don't want to run untrusted binaries, please consider one of the options
-above
+	 * checkout **clang** repository and build it yourself. To make it easy, 
+	 feel free to use [/checkout_build_install_llvm.sh](checkout_build_install_llvm.sh)
+	 script, it has been tested on clean Ubuntu 16.04
+	 * a VirtualBox VM with working environment will be provided at the workshop
+* `sudo apt-get install -y make autoconf automake libtool`
+
 
 Fuzzing experience is not required.
 
---
+++ checkout_build_install_llvm.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -eux
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+
+sudo apt-get install -y libc6-dev binutils libgcc-5-dev
+
+LLVM_DEP_PACKAGES="build-essential make cmake ninja-build git python2.7"
+sudo apt-get install -y $LLVM_DEP_PACKAGES
+
+WORK_DIR=$PWD
+mkdir -p $WORK_DIR/src
+
+# Checkout
+cd $WORK_DIR/src && git clone --depth 1 http://llvm.org/git/llvm.git
+cd $WORK_DIR/src/llvm/tools && git clone --depth 1 http://llvm.org/git/clang.git
+cd $WORK_DIR/src/llvm/projects && git clone --depth 1 http://llvm.org/git/compiler-rt.git
+cd $WORK_DIR/src/llvm/projects && git clone --depth 1 http://llvm.org/git/libcxx.git
+cd $WORK_DIR/src/llvm/projects && git clone --depth 1 http://llvm.org/git/libcxxabi.git
+
+# Build & Install
+mkdir -p $WORK_DIR/work/llvm
+cd $WORK_DIR/work/llvm
+cmake -G "Ninja" \
+      -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
+      -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" \
+      $WORK_DIR/src/llvm
+ninja -j$(nproc)
+sudo ninja install
+rm -rf $WORK_DIR/work/llvm
+
+
+#
+sudo apt-get install -y make autoconf automake libtool
+
+# Copy libfuzzer sources
+#mkdir /src/libfuzzer
+#cp -r /src/llvm/lib/Fuzzer/* /src/libfuzzer/
+
+#cp /src/llvm/tools/sancov/coverage-report-server.py /usr/local/bin/
+
+# Cleanup
+#rm -rf /src/llvm
